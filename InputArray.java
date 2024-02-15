642677d9e9bea61706f4ae05587b01bfdd7f8ee5
NithyaNN3 <nithyaniranjani@gmail.com>
2024-02-15 20:12:21 +1100

    word is NOW stored in an array and then displayed by position

--- InputArray.java
+++ InputArray.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class InputArray {
     public static void main(String[] args) {
-        Scanner wordOfTheDay = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the word of the day: ");
 
-        String word = wordOfTheDay.nextLine();
-        System.out.println("Word is: " + word);
+        String word = scanner.nextLine();
+        char[] wordArray = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            wordArray[i] = word.charAt(i);
+        }
+
+        System.out.println("Word stored in array: ");
+
+        for (int i = 0; i < wordArray.length; i++) {
+            System.out.println(wordArray[i]);
+        }
     }
 }
