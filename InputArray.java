087f40d41d0229f8df143a41d17eed30d318b047
NithyaNN3 <nithyaniranjani@gmail.com>
2024-02-16 23:47:34 +1100

    added wrong inputs in a loop

--- InputArray.java
+++ InputArray.java
@@ -11,25 +11,27 @@ public class InputArray {
 
         for (int i = 0; i < word.length(); i++) {
             wordArray[i] = word.charAt(i);
-        }
-
-        System.out.println("Enter your guess: ");
-
-        String userGuess = scanner.nextLine();
-        char[] guessArray = new char[userGuess.length()];
-
-        for (int i = 0; i < userGuess.length(); i++) {
-            guessArray[i] = userGuess.charAt(i);
-        }
-
-        boolean areEqual = Arrays.equals(wordArray, guessArray);
+        }   
 
         for (int i = 0; i < 5; i++) {
+            System.out.println("Enter your guess: ");
+
+            String userGuess = scanner.nextLine();
+            char[] guessArray = new char[userGuess.length()];
+
+            for (int j = 0; j < userGuess.length(); j++) {
+                guessArray[j] = userGuess.charAt(j);
+            }
+
+            boolean areEqual = Arrays.equals(wordArray, guessArray);
+
             if (areEqual) {
                 System.out.println("You've got it!");
                 break;
             } else {
-                System.out.println("This is not the word");
+                System.out.println("Not the word!");
+                continue;
             }
+        }
     }
 }
