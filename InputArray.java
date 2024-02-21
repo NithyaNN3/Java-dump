ff6ac1f84f8d9929d3a3e7987cb9cb6a88ebe7aa
NithyaNN3 <nithyaniranjani@gmail.com>
2024-02-21 19:12:13 +1100

    added logger method

--- InputArray.java
+++ InputArray.java
@@ -1,7 +1,38 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class Logger {
+    // constructor
+    // Log method, takes a string as input and prints to stdout if debug flag set
+    //
+}
 
 public class InputArray {
+
+// Method: Set secret word (make sure to sanitise input)
+// Method: Get user guess (make sure to sanitise guess input)
+// Method: Check guess
+// Method: Display guess feedback
+// Every file, class and method should have documentation using the JavaDoc format
+// Get a windows app version 
+// Move print statements to debug statements, which are only printed if run with a --debug or -d flag. Create a Logger class.
+
+
+    /* checkGuess
+     * @secret: Word of the day that user needs to guess
+     * @guess: Word provided by user
+     * @output:
+     */
+    private void checkGuess(String secret, String, guess) {
+        /*/ All comparison logic goes here
+        for (i = 0; i < secret.length(); i++) {
+            for (j = 0; j < guess.length(); j++) {
+                // Logic
+            }
+        }
+        //*/
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the word of the day: ");
@@ -27,6 +58,7 @@ public class InputArray {
 
             if (areEqual) {
                 System.out.println("You've got it!");
+                // Log.debug("You've got it!");
                 break;
             } else {
                 // Compare two arrays element by element
