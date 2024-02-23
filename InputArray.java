d32ca1b0b8759dd7a4950c9ff2d91b70c7551f32
NithyaNN3 <nithyaniranjani@gmail.com>
2024-02-23 22:06:50 +1100

    sanitised whitespaces from strings

--- InputArray.java
+++ InputArray.java
@@ -32,7 +32,7 @@ public class InputArray {
 
         String word = scanner.nextLine();
 
-        return word;
+        return word.trim();
     }
 
     private boolean isFiveLetters(String word) {
@@ -49,7 +49,7 @@ public class InputArray {
 
         String userGuess = scanner.nextLine();
 
-        return userGuess;
+        return userGuess.trim();
     }
 
     private void checkGuess(String word, String userGuess) {
